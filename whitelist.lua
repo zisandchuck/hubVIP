85be6db326b2c58ba41d8dae0dbaa8c9647fec76
zisandchuck <check868@163.com>
2026-02-01 08:21:52 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,4 +1,4 @@
 return {
-    allowedUsernames = {"checkzis", "zis96961", "gxv_gxg", "laobider123456", "cTc_acQ", "cTcacQ"}
+    allowedUsernames = {"checkzis", "zis96961", "gxv_gxg", "laobider123456", "cTc_acQ", "cTcacQ", "hdhdjd87648"}
 }
 
