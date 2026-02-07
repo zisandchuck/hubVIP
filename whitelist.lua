3c57605ab76c035b3d3fd69d1c783c8e07be1052
zisandchuck <check868@163.com>
2026-02-08 04:08:03 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,4 +1,4 @@
 return {
-    allowedUsernames = {"zis96961", "sjwuxnshi", "lamluoyi", "114514robloxt", "GHT_78091", "gxv_gxg", "laobider123456", "cTc_acQ", "cTcacQ", "vvvvssss88", "hdhdjd87648", "zis96967"}
+    allowedUsernames = {"zis96961", "zis9494", "sjwuxnshi", "lamluoyi", "114514robloxt", "GHT_78091", "gxv_gxg", "laobider123456", "cTc_acQ", "cTcacQ", "vvvvssss88", "hdhdjd87648", "zis96967"}
 }
 
