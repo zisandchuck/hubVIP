fdcb653d8de3daeb2473f5c909e21874b9977f4b
zisandchuck <check868@163.com>
2026-02-07 18:44:47 +0800

    Add 'GHT_78091' to allowedUsernames list

--- whitelist.lua
+++ whitelist.lua
@@ -1,4 +1,4 @@
 return {
-    allowedUsernames = {"zis96961", "sjwuxnshi", "114514robloxt", "gxv_gxg", "laobider123456", "cTc_acQ", "cTcacQ", "vvvvssss88", "hdhdjd87648", "zis96967"}
+    allowedUsernames = {"zis96961", "sjwuxnshi", "114514robloxt", "GHT_78091", "gxv_gxg", "laobider123456", "cTc_acQ", "cTcacQ", "vvvvssss88", "hdhdjd87648", "zis96967"}
 }
 
