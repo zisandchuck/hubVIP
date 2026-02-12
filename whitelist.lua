2ae0a65a7da37bd254254c96c1a98afbb3203794
zisandchuck <check868@163.com>
2026-02-12 08:20:42 +0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,15 +1,15 @@
 return {
-    {salt = "lN7fB2Zf89shvNhCMBwBnw==", hash = "6e1819c57708a311c4011dbc23ad8cca243beb894122c980c2bf71ed86c296aa"},
-    {salt = "n6XFdeMmRIKusaohrZAH0w==", hash = "ea9905694a51aef9a25742258bd120058a035f4df3df461dd9152d068bbe7f03"},
-    {salt = "1KLKpd/nL8zYbr0bIZHXdg==", hash = "6d20eb10daebd573d006c045e7966146da9b83b419e8146320981352f38a4541"},
-    {salt = "n11JHZB9sG5vMfV4kzm7zg==", hash = "11535f37ef62c2df553c1fbafadec1042f8c20af7286c42ffeb43d3e0253932c"},
-    {salt = "sbs0x1oCiSa134mq5R07+w==", hash = "9b7f70920f3aa910f7d1969e585baed1f677ec164352f16fd0418d687ac4e24f"},
-    {salt = "/gbo9AgmF9GgItDdO1kyow==", hash = "4a00c06c770a23d5797c15ee75b091a5c89800b2e0d10fa9cc33725bfa512de3"},
-    {salt = "0Ne4RvItBuOnE6X7Ihabfg==", hash = "5e74b7eeee8f4b9d1149e97d6abaab4dcbfb196ec6ea8d8b6bb182d3c525e2f5"},
-    {salt = "QRYDHJTZyxJqktEoZqxPtQ==", hash = "3de8ef74c441264bc7eb7a51da2318fb2ba0f9781771bb4b8da31a29126f964f"},
-    {salt = "NB8+In0TpZ6zkREiISqOGQ==", hash = "6d6867736778efcb321f45be921eccd0abfdb498e5fb74a4dd26179d770752d8"},
-    {salt = "zX0nvYCCgkoC8JhdTo6oJw==", hash = "0ab9d8d3906747927aa32034010c82b9add55b0fa1792bb9024edfd05618d562"},
-    {salt = "DssFU8fLT1g9hCAoQaSQHA==", hash = "5f1f9f4e1d4a9586e9c3c6430a26fb2f6621a8badf790af088219fc8c1180924"},
-    {salt = "USTidszruyzpCrXiqU8Fuw==", hash = "f7d442c03b8d43c0497e65154a8e19033c03d50019034d497a8619045bb0b0f2"},
-    {salt = "37F12GiF4igK0ADoxeLWCg==", hash = "eb22dbdca8ea29ef60841d7d58a52452561ad7634b5b778154f33a47b3f61d1c"}
+    {salt = "UXQuPSY4YCxVq2LP7hobvg==", hash = "e227686da4ae733033495a5501ff48713ee3f98b2d9cbf2e3f2436adeb0155ae"},
+    {salt = "xyciWlfJk+mQEVBqe61TVQ==", hash = "c115c35f82282f38e17d44b0f31d29b8b7458ccebc3b53c7399314116ffd5bbe"},
+    {salt = "B99Zpmkzxsb6i35KkjBw/g==", hash = "3c51cc8b3537cdb462cdf6463e54b4841d1ca5eab09023be02b008c1d53d4dc7"},
+    {salt = "BjSjr/HdFL0YPxrO7zBcag==", hash = "b25b0e0d5467b7f27861d6072a1bbccaf2dfb030cbe0fe3e6a942292b9e988c8"},
+    {salt = "ceDf6deI6q38sWtI3jrWqA==", hash = "2dc079b5df4e98357cc278f5b5835b353c097391907522e6dbafe31c0c4af973"},
+    {salt = "UuAk0uxZrd0WvW5KqvBilg==", hash = "dc2778330ad765355275fded88660217365e9966f48b475233c242274b815f03"},
+    {salt = "+boxY9w86/OlHFmPurcTdg==", hash = "d6566bcae121b5e11d8f9cde2f02e8e2f58e6cd100f282f222eb9648f89a960f"},
+    {salt = "GtTA/FLl+6BkJxyFu6y0iA==", hash = "27d66620d087de0ed79e3121a676e1631e26b186d4f0f344c4f8b5ca2c912ae5"},
+    {salt = "rU+T3X9lRQCnbgAkyWsRuA==", hash = "6c7a7a993edb6e41c29c4883ff8b257c49e63c9b07f499ba06c29d4d10e3d79b"},
+    {salt = "RfYa3ddud40dFdphZvnMgA==", hash = "5043a895a89faa7e14642f1aa33b15338434ffe3e4d2c85960ed4b8424d6b913"},
+    {salt = "qtDSZs7sEWABO2Z7Yk7lrA==", hash = "aac6665f03436f0e70be1b65f1d0ccba2e5ab44cb60ceafd0fdc9d8b0e77587d"},
+    {salt = "dALQldUwft3HtTBhAR5u0Q==", hash = "63910edd84c00f934bbde0410f00728c0d117e59239a6ef67513f0aeb93d4987"},
+    {salt = "9zpY1dTpT+PgLluL8ciP+g==", hash = "000ade6ad016c3a4172aad8b3bee23c0a6946e0787beb8ae3c6aaa7519a50032"}
 }
