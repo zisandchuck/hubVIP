4a21f7369e25ce7a7a73ba550907f4ee3999aa04
zisandchuck <check868@163.com>
2026-02-12 08:16:54 +0800

    Replace encrypted list with salt and hash pairs

--- whitelist.lua
+++ whitelist.lua
@@ -1,17 +1,15 @@
 return {
-    encryptedList = {
-        "MhwSV1ELfFg=",
-        "MhwSV1MLfg==",
-        "Ox8WGx9cOQFY",
-        "JBQMAhJdMwA=",
-        "eURVW1YGOAZTJBoaNA==",
-        "JhsMA1YAf1gA",
-        "Lw0XMQBKLQ==",
-        "JBQODA5WLxsAekZWdWA=",
-        "KyECMQZRGw==",
-        "KyECDwRj",
-        "PgMXGBRBORoJcA==",
-        "IBEJCg1Wcl4HfE0=",
-        "MhwSV1ELfF4="
-    }
+    {salt = "lN7fB2Zf89shvNhCMBwBnw==", hash = "6e1819c57708a311c4011dbc23ad8cca243beb894122c980c2bf71ed86c296aa"},
+    {salt = "n6XFdeMmRIKusaohrZAH0w==", hash = "ea9905694a51aef9a25742258bd120058a035f4df3df461dd9152d068bbe7f03"},
+    {salt = "1KLKpd/nL8zYbr0bIZHXdg==", hash = "6d20eb10daebd573d006c045e7966146da9b83b419e8146320981352f38a4541"},
+    {salt = "n11JHZB9sG5vMfV4kzm7zg==", hash = "11535f37ef62c2df553c1fbafadec1042f8c20af7286c42ffeb43d3e0253932c"},
+    {salt = "sbs0x1oCiSa134mq5R07+w==", hash = "9b7f70920f3aa910f7d1969e585baed1f677ec164352f16fd0418d687ac4e24f"},
+    {salt = "/gbo9AgmF9GgItDdO1kyow==", hash = "4a00c06c770a23d5797c15ee75b091a5c89800b2e0d10fa9cc33725bfa512de3"},
+    {salt = "0Ne4RvItBuOnE6X7Ihabfg==", hash = "5e74b7eeee8f4b9d1149e97d6abaab4dcbfb196ec6ea8d8b6bb182d3c525e2f5"},
+    {salt = "QRYDHJTZyxJqktEoZqxPtQ==", hash = "3de8ef74c441264bc7eb7a51da2318fb2ba0f9781771bb4b8da31a29126f964f"},
+    {salt = "NB8+In0TpZ6zkREiISqOGQ==", hash = "6d6867736778efcb321f45be921eccd0abfdb498e5fb74a4dd26179d770752d8"},
+    {salt = "zX0nvYCCgkoC8JhdTo6oJw==", hash = "0ab9d8d3906747927aa32034010c82b9add55b0fa1792bb9024edfd05618d562"},
+    {salt = "DssFU8fLT1g9hCAoQaSQHA==", hash = "5f1f9f4e1d4a9586e9c3c6430a26fb2f6621a8badf790af088219fc8c1180924"},
+    {salt = "USTidszruyzpCrXiqU8Fuw==", hash = "f7d442c03b8d43c0497e65154a8e19033c03d50019034d497a8619045bb0b0f2"},
+    {salt = "37F12GiF4igK0ADoxeLWCg==", hash = "eb22dbdca8ea29ef60841d7d58a52452561ad7634b5b778154f33a47b3f61d1c"}
 }
